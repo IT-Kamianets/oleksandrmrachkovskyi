a98c8a0301f5c7d8cb1312727d78e5367a1f2035
MrachkovskyiOleksandr <knms1b22.mrachkovskyi@gmail.com>
2025-09-09 20:36:27 +0300

    minor changes

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const currentYear = new Date().getFullYear();
 
-// Встановлюємо значення року в елемент із id "year"
 document.getElementById('year').textContent = currentYear;
 
 function calculateAge(birthDate) {
@@ -10,7 +9,6 @@ function calculateAge(birthDate) {
     const monthDiff = now.getMonth() - birth.getMonth();
     const dayDiff = now.getDate() - birth.getDate();
 
-    // Якщо місяць або день народження ще не настав цього року, то віднімаємо 1 рік
     if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
         age--;
     }
@@ -18,7 +16,7 @@ function calculateAge(birthDate) {
 }
 
 function updateAge() {
-    const birthDate = '2002-11-17';  // Заміни на свою дату народження
+    const birthDate = '2002-11-17';
     const ageElement = document.getElementById('age');
     const age = calculateAge(birthDate);
     ageElement.textContent = age;
